3f9b322d51d6a28be5ddd285ecd493f7ffc00853
Pradeep Khanal <pradeepkhanal642@gmail.com>
2024-01-20 21:54:25 +1100

    clock style changed

--- index.js
+++ index.js
@@ -22,23 +22,34 @@ function drawClock() {
   // Setting up canvas
   ctx.translate(centerX, centerY);
   ctx.rotate(-Math.PI / 2);
-  ctx.strokeStyle = "coral"; // Stroke color setup
-  ctx.fillStyle = "#f5f5f5"; // Inside the circle color
-  ctx.lineWidth = 3; // Stroke width
-  ctx.lineCap = "round"; // Rounded line at the edges
+
+  // Update colors and styles for the neumorphism style
+  ctx.fillStyle = "#0b1a21"; // Dark background color
+  ctx.shadowBlur = 0; // Disable the default shadow
 
   // Clock face/border
   ctx.save();
   ctx.beginPath();
-  ctx.lineWidth = 14;
   ctx.arc(0, 0, centerX - 10, 0, Math.PI * 2, true); // Outer circle
-  ctx.stroke();
+  ctx.fillStyle = "#0b1a21"; // Dark background color
+  ctx.shadowOffsetX = 5;
+  ctx.shadowOffsetY = 5;
+  ctx.shadowBlur = 0;
+  ctx.shadowColor = "#071015"; // Dark shadow color
+  ctx.fill();
+
+  ctx.beginPath();
+  ctx.arc(0, 0, centerX - 10, 0, Math.PI * 2, true); // Outer circle
+  ctx.fillStyle = "#0b1a21"; // Dark background color
+  ctx.shadowBlur = 10;
+  ctx.shadowColor = "#0f242d"; // Light shadow color
   ctx.fill();
   ctx.restore();
 
   // Drawing hour marks
   ctx.save();
   ctx.lineWidth = 8;
+  ctx.strokeStyle = "#6f6e75"; // Dark background color for the marks
   for (let i = 0; i < 12; i++) {
     ctx.beginPath();
     ctx.rotate(Math.PI / 6);
@@ -46,11 +57,13 @@ function drawClock() {
     ctx.lineTo(centerX - 10, 0);
     ctx.stroke();
   }
+
   ctx.restore();
 
   // Drawing minute marks
   ctx.save();
   ctx.lineWidth = 4;
+  ctx.strokeStyle = "#0b1a21"; // Dark background color for the marks
   for (let i = 0; i < 60; i++) {
     if (i % 5 !== 0) {
       ctx.beginPath();
@@ -81,6 +94,7 @@ function drawHourHand(hr, min) {
   const hourAngle = (Math.PI / 6) * hr + (Math.PI / 360) * min;
   ctx.rotate(hourAngle);
   ctx.lineWidth = 14;
+  ctx.strokeStyle = "#fd115d"; // Dark background color for the hand
   ctx.beginPath();
   ctx.moveTo(-20, 0);
   ctx.lineTo(70, 0);
@@ -93,7 +107,7 @@ function drawMinuteHand(min, sec) {
   const minAngle = (Math.PI / 30) * min + (Math.PI / 1800) * sec;
   ctx.rotate(minAngle);
   ctx.lineWidth = 10;
-  ctx.strokeStyle = "skyblue";
+  ctx.strokeStyle = "#276c9f"; // Dark background color for the hand
   ctx.beginPath();
   ctx.moveTo(-28, 0);
   ctx.lineTo(160, 0);
@@ -106,14 +120,14 @@ function drawSecondHand(sec) {
   const secondAngle = (sec * Math.PI) / 30;
   ctx.rotate(secondAngle);
   ctx.lineWidth = 7;
-  ctx.strokeStyle = "green";
+  ctx.strokeStyle = "white"; // Dark background color for the hand
   ctx.beginPath();
   ctx.moveTo(-30, 0);
   ctx.lineTo(165, 0);
   ctx.stroke();
 
   ctx.beginPath();
-  ctx.fillStyle = "green";
+  ctx.fillStyle = "white"; // Dark background color for the center of the second hand
   ctx.arc(0, 0, 10, 0, Math.PI * 2, true);
   ctx.fill();
   ctx.restore();
